f65bed1b43ad294f1c8f102d0a48150e3656038e
its-filip-melka <133359834+its-filip-melka@users.noreply.github.com>
2024-06-03 17:29:28 +0200

    Display next question

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const CATEGORY = document.getElementById('category')
 const QUESTION = document.getElementById('question')
 const OPTIONS = document.getElementById('options')
+const NEXT_BTN = document.getElementById('next-btn')
 
 let correctAnswerIndex
 
 async function nextQuestion() {
+	// hide 'next' button
+	NEXT_BTN.className = 'hidden'
+
 	const res = await fetch('https://opentdb.com/api.php?amount=1&type=multiple')
 	if (res.ok) {
 		const data = await res.json()
@@ -20,6 +24,11 @@ async function nextQuestion() {
 		correctAnswerIndex = Math.round(Math.random() * incorrect_answers.length)
 		options.splice(correctAnswerIndex, 0, correct_answer)
 
+		// remove classes from all option buttons
+		removeClassesFromOptions()
+		// enable option buttons
+		OPTIONS.className = ''
+
 		// insert the text into the option buttons and add event listeners
 		for (let i = 0; i < options.length; i++) {
 			const optionBtn = document.getElementById(`option-${i}`)
@@ -30,13 +39,24 @@ async function nextQuestion() {
 }
 
 function handleClick(index) {
+	// disable option buttons
 	OPTIONS.className = 'disabled'
+
 	if (index === correctAnswerIndex) {
 		document.getElementById(`option-${index}`).className = 'correct'
 	} else {
 		document.getElementById(`option-${index}`).className = 'incorrect'
 		document.getElementById(`option-${correctAnswerIndex}`).className =
 			'correct'
+	}
+
+	// show 'next' button
+	NEXT_BTN.className = ''
+}
+
+function removeClassesFromOptions() {
+	for (let i = 0; i < OPTIONS.childNodes.length; i++) {
+		OPTIONS.childNodes[i].className = ''
 	}
 }
 
